7ecb14f44c59f9792d670f0601ae42c44a183680
Watthana1132 <boxgieking1132@gmail.com>
2026-04-24 12:31:17 +0700

    4

--- dodge-sword-mod-template-26.1.2/src/main/java/com/rushberry/dodgesword/DodgeSwordMod.java
+++ dodge-sword-mod-template-26.1.2/src/main/java/com/rushberry/dodgesword/DodgeSwordMod.java
@@ -1,9 +1,9 @@
 package com.rushberry.dodgesword;
 
-import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
 
 public class DodgeSwordMod implements ModInitializer {
 	public static final String MOD_ID = "dodgesword";
@@ -12,6 +12,7 @@ public class DodgeSwordMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	
 
 	@Override
 	public void onInitialize() {
